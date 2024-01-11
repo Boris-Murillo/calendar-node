7976f97e8808cfd197daf8d6f7de1af5faf163fa
Boris-Murillo <murilloboris97@gmail.com>
2024-01-10 21:13:47 -0500

    test route

--- src/app.js
+++ src/app.js
@@ -4,11 +4,16 @@ const cors = require('cors');
 const app = express();
 const router = require('./routes');
 
-app.use(cors());
-app.use(router);
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    });
+app.listen(3000, () => console.log('Server running on port 3000'));
 
-const PORT = process.env.PORT || 3000;
+// app.use(cors());
+// app.use(router);
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
